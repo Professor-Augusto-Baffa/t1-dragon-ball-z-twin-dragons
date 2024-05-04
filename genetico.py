2a33711b210cf02b6f360d94dd9fea545dfc7794
lumpleme <108637505+lumpleme@users.noreply.github.com>
2024-05-03 21:49:40 -0300

    Ajustes para aumentar % de acerto

--- genetico.py
+++ genetico.py
@@ -15,7 +15,7 @@ def tempoTotal(ind, batalhas, herois):
     return tempo
 
 def fitness(ind, batalhas, herois):
-    valor = 1.5 ** (sum(batalhas) - tempoTotal(ind, batalhas, herois))
+    valor = 1.4 ** (sum(batalhas) - tempoTotal(ind, batalhas, herois))
     if (not verificaAnt(ind, batalhas, herois)):
         return valor
     return valor * 100
@@ -42,9 +42,11 @@ def verificaAnt(ind, batalhas, herois):
     return True
 
 def hillClimbing(melhor, batalhas, herois):
+
     melhorAnt = melhor.copy()
     melhorTotal = melhor.copy()
-    for n in range(2000):
+
+    for n in range(5000):
 
         mutado = mutacao(copiaListaDeListas(melhor[1]), batalhas)
 
@@ -53,7 +55,7 @@ def hillClimbing(melhor, batalhas, herois):
             for i in range(num):
                 mutado = mutacao(copiaListaDeListas(mutado), batalhas)
 
-        if (n % 30 == 0):
+        if (n % 25 == 0):
             if (melhor == melhorAnt):
                 melhor[1] = copiaListaDeListas(mutado)
                 melhor[0] = fitness(melhor[1], batalhas, herois)
@@ -81,15 +83,11 @@ def randomGen(qtd, batalhas, herois):
 
             ind = []
             verificacao = batalhas.copy()
-            h = random.sample(batalhas, 4)
-            h.sort()
-            ind.append(h)
+            ind.append(random.sample(batalhas, 4))
             verificacao = set(verificacao) - set(ind[0])
             
             for i in range(len(herois) - 1):
-                h = random.sample(batalhas, 5)
-                h.sort()
-                ind.append(h)
+                ind.append(random.sample(batalhas, 5))
                 verificacao = set(verificacao) - set(ind[i+1])
 
         geracao.append([fitness(ind, batalhas, herois), ind])
@@ -167,69 +165,30 @@ def evolucao(ngens, indsgen, geracao, batalhas, herois):
 
 batalhas = [35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95]
 herois = [1.1, 1.2, 1.3, 1.4, 1.5]
-ngens = 1500
+ngens = 1600
 indsgen = 300
 batalhas.sort()
 herois.sort()
 
-melhores = []
-#melhores.append([fitness([[45, 60, 75, 50], [55, 65, 85, 70, 60], [50, 90, 80, 95, 45], [85, 35, 90, 55, 95], [70, 75, 65, 80, 40]], batalhas, herois), [[45, 60, 75, 50], [55, 65, 85, 70, 60], [50, 90, 80, 95, 45], [85, 35, 90, 55, 95], [70, 75, 65, 80, 40]]])
-#melhores.append([fitness([[50, 70, 85, 55], [80, 75, 50, 45, 95], [90, 45, 65, 60, 55], [35, 75, 60, 40, 65], [70, 80, 90, 95, 85]], batalhas, herois), [[50, 70, 85, 55], [80, 75, 50, 45, 95], [90, 45, 65, 60, 55], [35, 75, 60, 40, 65], [70, 80, 90, 95, 85]]])
-#melhores.append([fitness([[55, 50, 45, 90], [45, 60, 75, 70, 55], [65, 85, 60, 50, 90], [85, 70, 95, 80, 35], [95, 75, 40, 65, 80]], batalhas, herois), [[55, 50, 45, 90], [45, 60, 75, 70, 55], [65, 85, 60, 50, 90], [85, 70, 95, 80, 35], [95, 75, 40, 65, 80]]])
-#melhores.append([fitness([[50, 70, 65, 75], [50, 55, 45, 85, 60], [45, 55, 80, 95, 75], [40, 60, 80, 90, 95], [90, 35, 65, 85, 70]], batalhas, herois), [[50, 70, 65, 75], [50, 55, 45, 85, 60], [45, 55, 80, 95, 75], [40, 60, 80, 90, 95], [90, 35, 65, 85, 70]]])
-#melhores.append([fitness([[80, 60, 50, 45], [65, 85, 70, 55, 75], [45, 65, 50, 80, 90], [95, 90, 35, 60, 70], [55, 95, 75, 40, 85]], batalhas, herois), [[80, 60, 50, 45], [65, 85, 70, 55, 75], [45, 65, 50, 80, 90], [95, 90, 35, 60, 70], [55, 95, 75, 40, 85]]])
-#melhores.append([fitness([[65, 75, 45, 85], [55, 45, 95, 90, 60], [55, 80, 65, 50, 70], [90, 50, 75, 95, 70], [80, 35, 60, 40, 85]], batalhas, herois), [[65, 75, 45, 85], [55, 45, 95, 90, 60], [55, 80, 65, 50, 70], [90, 50, 75, 95, 70], [80, 35, 60, 40, 85]]])
-#melhores.append([fitness([[65, 45, 60, 50], [50, 90, 75, 45, 60], [80, 55, 70, 85, 40], [80, 55, 95, 70, 90], [75, 65, 85, 95, 35]], batalhas, herois), [[65, 45, 60, 50], [50, 90, 75, 45, 60], [80, 55, 70, 85, 40], [80, 55, 95, 70, 90], [75, 65, 85, 95, 35]]])
-#melhores.append([fitness([[65, 70, 40, 55], [60, 40, 55, 75, 95], [50, 70, 90, 60, 80], [50, 65, 90, 85, 80], [95, 45, 85, 35, 75]], batalhas, herois), [[65, 70, 40, 55], [60, 40, 55, 75, 95], [50, 70, 90, 60, 80], [50, 65, 90, 85, 80], [95, 45, 85, 35, 75]]])
-#melhores.append([fitness([[90, 55, 60, 45], [75, 55, 50, 85, 65], [65, 80, 95, 90, 45], [95, 50, 85, 80, 70], [70, 40, 60, 35, 75]], batalhas, herois), [[90, 55, 60, 45], [75, 55, 50, 85, 65], [65, 80, 95, 90, 45], [95, 50, 85, 80, 70], [70, 40, 60, 35, 75]]])
-#melhores.append([fitness([[55, 50, 45, 60], [75, 45, 70, 60, 50], [65, 90, 80, 95, 85], [80, 85, 90, 70, 55], [40, 35, 65, 95, 75]], batalhas, herois), [[55, 50, 45, 60], [75, 45, 70, 60, 50], [65, 90, 80, 95, 85], [80, 85, 90, 70, 55], [40, 35, 65, 95, 75]]])
-#melhores.append([fitness([[50, 80, 55, 75], [65, 60, 40, 85, 55], [65, 40, 90, 70, 50], [95, 75, 85, 90, 70], [45, 80, 35, 95, 60]], batalhas, herois), [[50, 80, 55, 75], [65, 60, 40, 85, 55], [65, 40, 90, 70, 50], [95, 75, 85, 90, 70], [45, 80, 35, 95, 60]]])
-#melhores.append([fitness([[45, 65, 55, 50], [75, 45, 70, 60, 50], [80, 90, 70, 85, 60], [55, 85, 90, 80, 95], [40, 35, 65, 95, 75]], batalhas, herois), [[45, 65, 55, 50], [75, 45, 70, 60, 50], [80, 90, 70, 85, 60], [55, 85, 90, 80, 95], [40, 35, 65, 95, 75]]])
-#melhores.append([fitness([[45, 65, 55, 50], [75, 45, 70, 60, 50], [70, 85, 55, 90, 80], [60, 80, 95, 85, 90], [40, 35, 65, 95, 75]], batalhas, herois), [[45, 65, 55, 50], [75, 45, 70, 60, 50], [70, 85, 55, 90, 80], [60, 80, 95, 85, 90], [40, 35, 65, 95, 75]]])
-#melhores.append([fitness([[55, 50, 45, 60], [65, 50, 75, 45, 70], [70, 85, 55, 90, 80], [60, 80, 95, 85, 90], [40, 35, 65, 95, 75]], batalhas, herois), [[55, 50, 45, 60], [65, 50, 75, 45, 70], [70, 85, 55, 90, 80], [60, 80, 95, 85, 90], [40, 35, 65, 95, 75]]])
-#melhores.append([fitness([[55, 50, 45, 60], [65, 50, 75, 45, 70], [65, 80, 70, 55, 85], [60, 80, 95, 85, 90], [90, 95, 35, 75, 40]], batalhas, herois), [[55, 50, 45, 60], [65, 50, 75, 45, 70], [65, 80, 70, 55, 85], [60, 80, 95, 85, 90], [90, 95, 35, 75, 40]]])
-#melhores.append([fitness(, batalhas, herois), ])
-#melhores.append([fitness(, batalhas, herois), ])
-#melhores.append([fitness(, batalhas, herois), ])
-#melhores.append([fitness(, batalhas, herois), ])
-
-
-
 geracao = randomGen(indsgen, batalhas, herois)
-geracao.extend(melhores)
 geracao1 = evolucao(ngens, indsgen, geracao, batalhas, herois)
-print(geracao1[0][0])
+#print(geracao1[0][0])
 geracao1[0] = hillClimbing(geracao1[0], batalhas, herois)
-print(geracao1[0][0])
+#print(geracao1[0][0])
 geracao = randomGen(indsgen, batalhas, herois)
-geracao.extend(melhores)
 geracao2 = evolucao(ngens, indsgen, geracao, batalhas, herois)
-print(geracao2[0][0])
+#print(geracao2[0][0])
 geracao2[0] = hillClimbing(geracao2[0], batalhas, herois)
-print(geracao2[0][0])
-geracao = randomGen(indsgen, batalhas, herois)
-geracao.extend(melhores)
-geracao3 = evolucao(ngens, indsgen, geracao, batalhas, herois)
-print(geracao3[0][0])
-geracao3[0] = hillClimbing(geracao3[0], batalhas, herois)
-print(geracao3[0][0])
-geracao = randomGen(indsgen, batalhas, herois)
-geracao.extend(melhores)
-geracao4 = evolucao(ngens, indsgen, geracao, batalhas, herois)
-print(geracao4[0][0])
-geracao4[0] = hillClimbing(geracao4[0], batalhas, herois)
-print(geracao4[0][0])
+#print(geracao2[0][0])
 
 
-geracaofinal = geracao1[:int(indsgen*0.25)]
-geracaofinal.extend(geracao2[:int(indsgen*0.25)])
-geracaofinal.extend(geracao3[:int(indsgen*0.25)])
-geracaofinal.extend(geracao4[:int(indsgen*0.25)])
+geracaofinal = geracao1[:int(indsgen*0.5)]
+geracaofinal.extend(geracao2[:int(indsgen*0.5)])
 
 geracaofinal = evolucao(ngens, indsgen, geracaofinal, batalhas, herois)
-print("Ultimo da ultima geracao")
-print(geracaofinal[0][1])
-print(tempoTotal(geracaofinal[0][1], batalhas, herois))
+# print("Ultimo da ultima geracao")
+# print(geracaofinal[0][1])
+# print(tempoTotal(geracaofinal[0][1], batalhas, herois))
 
 melhor = hillClimbing(geracaofinal[0], batalhas, herois)
 
